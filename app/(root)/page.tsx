17365753571ff9da5f64238e48ac3290ecab57a4
chun-eric <eric.chun88800888@gmail.com>
2024-04-21 09:56:23 +0900

    finished footer, started hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,18 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <>
+      <section className='bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10'>
+        <div className='wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0'>
+          <div className='flex flex-col justify-center gap-8'>
+            <h1>Meet, Connect, Thrive: Your Meetups, Our Platform!</h1>
+            <p>
+              Meet and Learn from 5,000+ expert mentors at top global companies
+              in our dynamic community.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
