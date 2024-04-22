1befa6c61346e667ad1b305ad8b791d4b71258f1
chun-eric <eric.chun88800888@gmail.com>
2024-04-22 17:27:49 +0900

    connected to mongodb and started on the first user schema

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -6,4 +6,17 @@ let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URI) throw new Error("MongoDB URI is missing");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "meetly",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 };
---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,7 @@
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema({
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  username: {},
+});
